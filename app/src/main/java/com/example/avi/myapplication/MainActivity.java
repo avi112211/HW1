34c7d3124f2a05d58fc2b8229ded867292efdeee
Avi <avi.elgal@gmail.com>
2016-11-13 19:59:53 +0200

    first commit

--- app/src/main/java/com/example/avi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/avi/myapplication/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.avi.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit;
+    TextView text;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit = (EditText) findViewById(R.id.editText);
+        text = (TextView) findViewById(R.id.textView4);
+
+        ////////option 1///////
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText(edit.getText());
+            }
+        });
     }
+
+    /////option 2 with xml///////////
+    /*public void onClick(View v)
+    {
+        text.setText(edit.getText());
+    }*/
 }
